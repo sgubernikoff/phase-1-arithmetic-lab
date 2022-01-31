729f1c1d4e861fd897b14b8372679a3d6d716734
Sam Gubernikoff <sgubernikoff@gmail.com>
2022-01-31 09:26:18 -0500

    completed assignment

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const multiply = num1 * num2;
 let random = Math.floor(Math.random() * 10) + 1;
 console.log(random);
 
-let num3 = 10;
+let num3 = 24;
 let num4 = 6;
-const mod = num3 % num4;
+const mod = num3 / num4;
 
 let max = Math.max(1, 2, 3, 4, 20);
